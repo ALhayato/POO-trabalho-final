f4c71cfd11a08ed0e2308de41e8405371fa1613b
ADavidSilva <david.silva@alu.ufc.br>
2026-06-09 20:31:53 -0300

    Adicionado o Construtor, Getters e Setters da classe Pessoa

--- Pessoa.java
+++ Pessoa.java
@@ -1,8 +1,48 @@
 abstract class Pessoa implements Autenticavel{
+
     private int id;
     private String nome;
     private String cpf;
     private String senha;
     
-    abstract void exibirDados();
+    public Pessoa(int id, String nome, String cpf, String senha) {
+        this.cpf = cpf;
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public abstract void exibirDados();
 } 
