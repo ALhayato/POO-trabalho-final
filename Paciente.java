6fca2001da555e9a34e1372ede5cb0e57c38abdd
ALhayato <107962348+ALhayato@users.noreply.github.com>
2026-06-10 20:39:24 -0300

    Create Paciente.java

---
+++ Paciente.java
@@ -0,0 +1,16 @@
+class pacient extends Pessoa{
+  private String responsavelLegal;
+  private String telefoneResponsavel;
+  private String[] historicoClinico;
+
+  public void adicionarEvolucao(String texto, Funcionario autor){
+    
+  }
+  public String[] getHistoricoClinico(){
+    
+  }
+  @Override
+  public void exibirDados(){
+    
+  }
+}
