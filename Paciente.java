9e926b65f863b24ff252b067543438d44f4e9af7
ALhayato <107962348+ALhayato@users.noreply.github.com>
2026-06-11 20:51:31 -0300

    Update Paciente.java

--- Paciente.java
+++ Paciente.java
@@ -1,14 +1,15 @@
 class pacient extends Pessoa{
   private String responsavelLegal;
   private String telefoneResponsavel;
-  private String[] historicoClinico;
+  private List<String> historicoClinico;
 
-  public Paciente(String responsavelLegal, String telefoneResponsavel){
-    super.Pessoa(int id, String nome, String cpf, String senha);
+  public Paciente(int id, String nome, String cpf, String senha, String responsavelLegal, String telefoneResponsavel){
+    super(id, nome, cpf, senha);
     if(super.getIdade() < 18){
       this.responsavel = responsavel;
       this.telefoneResponsavel = telefoneResponsavel;
     }
+    this.historicoClinico = new ArrayList<String>();
   }
   public void adicionarEvolucao(String texto, Funcionario autor){
     
