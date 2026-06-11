c3d1de564c5def6d3578e354d1cd04e272d824ff
ALhayato <107962348+ALhayato@users.noreply.github.com>
2026-06-10 21:24:23 -0300

    Update Psiquiatra.java

--- Psiquiatra.java
+++ Psiquiatra.java
@@ -3,6 +3,12 @@ class Psiquiatra extends Funcionario{
   private String registroConselho;
 
   public Psiquiatra(String registroConselho){
-    super.Funcionario()
+    super.Funcionario(String cargo, String matricula, int id, String nome, String cpf, String senha);
+    this.registroConselho = registroConselho;
+  }
+
+  public void evoluirProntuario(Paciente p, String anotacao){
+    System.out.println("%s", anotacao);
+    
   }
 }
