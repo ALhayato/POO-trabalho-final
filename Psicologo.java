de63c3cc4e3c64dad7aea68812aec395f7637366
ALhayato <107962348+ALhayato@users.noreply.github.com>
2026-06-18 21:42:12 -0300

    Update Psicologo.java

--- Psicologo.java
+++ Psicologo.java
@@ -1,5 +1,17 @@
 class Psicologo extends Funcionario{
+  
   Private String especialidade;
 
-  public Psicologo()
+  public Psicologo(int id, String nome, String cpf, int idade, String senha, String especialidade){
+    super(id, nome, cpf, idade, senha);
+    this.especialidade = especialidade;
+  }
+
+  public boolean fazerVideochamada(Paciente p){
+    
+  }
+
+  public void evoluirProntuario(Paciente p, String anotacao){
+    
+  }
 }
