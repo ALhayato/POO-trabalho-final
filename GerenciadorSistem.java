1e8bf3b84a69f4e32ca5f6c401c593037398cf1d
ALhayato <107962348+ALhayato@users.noreply.github.com>
2026-06-22 21:57:17 -0300

    Create GerenciadorSistem.java

---
+++ GerenciadorSistem.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class GerenciadorSistema{
+
+
+  private list<Pessoa> usuarios = new Arraylist<>();
+  private list<Aviso> muralAvisos = new Arraylist<>();
+
+  public void cadastrarUsuario(Pessoa p){
+    
+  }
+
+  public Pessoa fazerLogin(String cpf, String senha){
+    
+  }
+
+  public List<Aviso>listarAvisosAtivos(){
+    
+  }
+}
