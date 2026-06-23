8f0c1a6f6efa5d31ceb1501560febba0a5a40c11
ADavidSilva <david.silva@alu.ufc.br>
2026-06-23 20:39:33 -0300

    Implementado o método cadastrarUsuario

--- GerenciadorSistem.java
+++ GerenciadorSistem.java
@@ -6,14 +6,25 @@ class GerenciadorSistema{
   private List<Aviso> muralAvisos = new ArrayList<>();
 
   public void cadastrarUsuario(Pessoa p){
-    
+    if(p == null){
+      throw new IllegalArgumentException("Não é possivel cadastrar um usuário nulo");
+    }
+
+    for(Pessoa u : usuarios){
+      if(u.getCpf().equals(p.getCpf())){
+        throw new IllegalArgumentException("Já existe um usuário cadastrado com esse CPF");
+      }
+    }
+
+    usuarios.add(p);
+    System.out.println("Usuário cadastrado com sucesso");
   }
 
   public Pessoa fazerLogin(String cpf, String senha){
     return;
   }
 
-  public List<Aviso>listarAvisosAtivos(){
+  public List<Aviso> listarAvisosAtivos(){
     return;
   }
 }
